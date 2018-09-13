b158dce8ec949b87d2f7f921ada43d41c2bc0824
Jon McCarthy <jtmcc93@gmail.com>
2018-09-13 14:34:01 -0700

    finish up to challenge 3 on part2

--- part2/server.js
+++ part2/server.js
@@ -1,9 +1,37 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 var port = process.env.PORT || 8000;
+var fs = require("fs");
+var bodyParser = require("body-parser");
 
-app.get('/yourroute', function(req, res) {
+app.use(bodyParser.json());
+
+app.get("/yourroute", function(req, res) {
   res.send("stuff");
+});
+
+app.post("/create/:name/:age", (req, res) => {
+  let user = {
+    name: req.params.name,
+    age: req.params.age
+  };
+  let filePath = __dirname + "/storage.json";
+  // Read file content && Convert into a js array (JSON.parse)
+  let fileContent = JSON.parse(fs.readFileSync(filePath, "utf8"));
+  fileContent.push(user);
+  fs.writeFileSync(filePath, JSON.stringify(fileContent));
+  res.send(fileContent);
+});
+
+app.get("/", (req, res) => {
+  res.send(fs.readFileSync(__dirname + "/storage.json", "utf8"));
+});
+
+app.get("/:name", (req, res) => {
+  let filePath = __dirname + "/storage.json";
+  let fileContent = JSON.parse(fs.readFileSync(filePath, "utf8"));
+  let name = fileContent.find(el => el.name === req.params.name);
+  name ? res.send(name) : res.sendStatus(400);
 });
 
 app.use(function(req, res) {
@@ -11,5 +39,5 @@ app.use(function(req, res) {
 });
 
 app.listen(port, function() {
-  console.log('Listening on port', port);
+  console.log("Listening on port", port);
 });
