b5252e4c52629a59c5406394459fa1794081d68d
Jon McCarthy <jtmcc93@gmail.com>
2018-09-13 13:20:59 -0700

    create package.json, finish part 1

--- part1/server.js
+++ part1/server.js
@@ -1,9 +1,29 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 var port = process.env.PORT || 8000;
 
-app.get('/yourroute', function(req, res) {
+app.get("/yourroute", function(req, res) {
   res.send("stuff");
+});
+
+app.get("/hello", (req, res) => {
+  res.send("Hello!");
+});
+
+app.post("/create/:name", (req, res) => {
+  let obj = {
+    id: 1,
+    name: req.params.name
+  };
+  res.json(obj);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/verify/:age", (req, res) => {
+  req.params.age >= 13 ? res.sendStatus(200) : res.sendStatus(403);
 });
 
 app.use(function(req, res) {
@@ -11,5 +31,5 @@ app.use(function(req, res) {
 });
 
 app.listen(port, function() {
-  console.log('Listening on port', port);
+  console.log("Listening on port", port);
 });
